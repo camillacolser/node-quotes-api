265e19878e9b95072cbe807d0394c38e34be6a4c
camilla.colomboserri <camilla.colomboserri@wallapop.com>
2022-11-29 19:40:32 +0100

    Implemented get, post, put, delete

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
-const quotes = require("./quotes.json");
+let quotes = require("./quotes.json");
+
+const middleware = express.json()
+
+app.use(middleware)
 
 app.get('/', function(request, response) {
   response.send('/quotes/17 should return one quote, by id')
@@ -10,5 +14,48 @@ app.get('/', function(request, response) {
 app.get("/quotes", function(request, response){
   response.json(quotes);
 });
+
+app.post('/quotes', function(request, response) {
+  const quote = request.body
+
+  if (notValidRequest(quote)) {
+    return response.status(400).send({success: false})
+  }
+
+  quotes.push(quote)
+  response.status(201).send({success: true})
+})
+
+function notValidRequest(quote) {
+  return quote.id == undefined ||
+  quote.author == undefined || 
+  quote.body == undefined
+}
+
+app.put('/quotes/:id', function(request, response) {
+  const id = request.params.id  
+  const quoteUpdated = {
+    id: parseInt(id),
+    author: request.body.author,
+    quote: request.body.quote
+  }
+  const quotesFiltered = quotes.filter(function (quote) {
+      return quote.id != id
+  })
+  quotesFiltered.push(quoteUpdated)
+  quotes = quotesFiltered
+
+  response.send({success: true})
+})
+
+app.delete('/quotes/:id', function(request, response) {
+  const id = request.params.id
+  const quotesFiltered = quotes.filter(function (quote) {
+    return quote.id != id
+  })
+  quotes = quotesFiltered
+
+  response.send({success: true})
+})
 
 app.listen(3000, () => console.log("Listening on port 3000"));
